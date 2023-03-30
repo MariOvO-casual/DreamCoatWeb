b5d9fc1a17d1301e1cc1b8a9ba229d0c1d23c5b8
Ruiyang Liu <ruiyangliu@ThinkPappleChip.lan>
2023-03-30 16:40:50 -0700

    added more info in registration

--- staybookingRef/staybooking/src/main/java/com/example/staybooking/model/User.java
+++ staybookingRef/staybooking/src/main/java/com/example/staybooking/model/User.java
@@ -1,5 +1,6 @@
 package com.example.staybooking.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -9,13 +10,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-// json 是数据库和java的纽带
-
 @Entity
 @Table(name = "user")
-@JsonDeserialize(builder = User.Builder.class) // jackson library for converting Json to Java object
-// 网络传输使用serializable，优化为网络可以传输的格式
-// 从前端收到的request 所以需要serializable，然后去mapping json
+@JsonDeserialize(builder = User.Builder.class)
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -24,28 +21,39 @@ public class User implements Serializable {
     private String password;
     @JsonIgnore
     private boolean enabled;
-    // for hibernate,
-    public User() {}
 
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "additional_info")
+    private String additionalInfo;
+
+    // for hibernate initializer
+    public User() {}
+    // initializer using builder
     private User(Builder builder) {
         this.username = builder.username;
         this.password = builder.password;
         this.enabled = builder.enabled;
+        this.companyName = builder.companyName;
+        this.address = builder.address;
+        this.email = builder.email;
+        this.phone = builder.phone;
+        this.additionalInfo = builder.additionalInfo;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-
-
+    // setter
     public User setUsername(String username) {
         this.username = username;
         return this;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public User setPassword(String password) {
@@ -53,15 +61,45 @@ public class User implements Serializable {
         return this;
     }
 
-    public boolean isEnabled() {
-        return enabled;
-    }
-
     public User setEnabled(boolean enabled) {
         this.enabled = enabled;
         return this;
     }
 
+    public User setCompanyName(String companyName) {
+        this.companyName = companyName;
+        return this;
+    }
+
+    public User setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+    public User setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+    public User setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+        return this;
+    }
+
+    // getters
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public boolean isEnabled() { return enabled; }
+    public String getCompanyName() { return companyName; }
+    public String getAddress() { return address; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+    public String getAdditionalInfo() { return additionalInfo; }
+
+    // builder
     public static class Builder {
         @JsonProperty("username")
         private String username;
@@ -71,6 +109,21 @@ public class User implements Serializable {
 
         @JsonProperty("enabled")
         private boolean enabled;
+
+        @JsonProperty("company_name")
+        private String companyName;
+
+        @JsonProperty("address")
+        private String address;
+
+        @JsonProperty("email")
+        private String email;
+
+        @JsonProperty("phone")
+        private String phone;
+
+        @JsonProperty("additional_info")
+        private String additionalInfo;
 
         public Builder setUsername(String username) {
             this.username = username;
@@ -87,9 +140,33 @@ public class User implements Serializable {
             return this;
         }
 
+        public Builder setCompanyName(String companyName) {
+            this.companyName = companyName;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setAdditionalInfo(String additionalInfo) {
+            this.additionalInfo = additionalInfo;
+            return this;
+        }
+
         public User build() {
             return new User(this);
         }
     }
-
 }
--- staybookingRef/staybooking/src/main/java/com/example/staybooking/repository/AuthorityRepository.java
+++ staybookingRef/staybooking/src/main/java/com/example/staybooking/repository/AuthorityRepository.java
@@ -4,6 +4,5 @@ import com.example.staybooking.model.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface AuthorityRepository extends JpaRepository<Authority,
-        String> {
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
 }
